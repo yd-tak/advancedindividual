43909594666e254c083b8783418a8984e13fbef3
yd-tak <yudhistiradewanata@gmail.com>
2024-07-19 13:08:34 +0700

    update v0.21

--- application/views/credit/statements.php
+++ application/views/credit/statements.php
@@ -14,53 +14,6 @@
 				<!--begin::Card title-->
 				<!--begin::Card toolbar-->
 				<div class="card-toolbar">
-					<!--begin::Toolbar-->
-					<div class="d-flex justify-content-end" data-kt-customer-table-toolbar="base">
-						<!--begin::Filter-->
-						<button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-						<i class="ki-duotone ki-filter fs-2">
-							<span class="path1"></span>
-							<span class="path2"></span>
-						</i>Filter</button>
-						<!--begin::Menu 1-->
-						<div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true" id="kt-toolbar-filter">
-							<!--begin::Header-->
-							<div class="px-7 py-5">
-								<div class="fs-4 text-dark fw-bold">Filter Options</div>
-							</div>
-							<!--end::Header-->
-							<!--begin::Separator-->
-							<div class="separator border-gray-200"></div>
-							<!--end::Separator-->
-							<!--begin::Content-->
-							<div class="px-7 py-5">
-								<div class="mb-10">
-									<label class="form-label fs-5 fw-semibold mb-3">Name:</label>
-									<input class="form-control fw-bold" name="name">
-								</div>
-								<div class="mb-10">
-									<label class="form-label fs-5 fw-semibold mb-3">Skills:</label>
-									<select class="form-select form-select-solid fw-bold" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-customer-table-filter="month" data-dropdown-parent="#kt-toolbar-filter">
-										<option></option>
-										<option value="aug">August</option>
-										<option value="sep">September</option>
-										<option value="oct">October</option>
-										<option value="nov">November</option>
-										<option value="dec">December</option>
-									</select>
-								</div>
-								<div class="d-flex justify-content-end">
-									<button type="reset" class="btn btn-light btn-active-light-primary me-2" data-kt-menu-dismiss="true" data-kt-customer-table-filter="reset">Reset</button>
-									<button type="submit" class="btn btn-primary" data-kt-menu-dismiss="true" data-kt-customer-table-filter="filter">Apply</button>
-								</div><!--end::Actions-->
-							</div>
-							<!--end::Content-->
-						</div>
-						<!--end::Menu 1-->
-						<!--end::Filter-->
-						
-					</div>
-					<!--end::Toolbar-->
 					<!--begin::Group actions-->
 					<div class="d-flex justify-content-end align-items-center d-none" data-kt-customer-table-toolbar="selected">
 						<div class="fw-bold me-5">
